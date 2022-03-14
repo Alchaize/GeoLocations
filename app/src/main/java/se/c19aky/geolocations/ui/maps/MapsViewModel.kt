021761520e3fab862835472f7e266613b0eef106
Alkade <alexander.yay.wow@gmail.com>
2022-03-14 20:11:33 +0100

    Removed home view and added map view

--- app/src/main/java/se/c19aky/geolocations/ui/maps/MapsViewModel.kt
+++ app/src/main/java/se/c19aky/geolocations/ui/maps/MapsViewModel.kt
@@ -1,13 +1,13 @@
-package se.c19aky.geolocations.ui.home
+package se.c19aky.geolocations.ui.maps
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class MapsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is maps Fragment"
     }
     val text: LiveData<String> = _text
 }
